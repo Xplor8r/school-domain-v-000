cdd6647350aaebccf0db669ec4ab0de1b624d449
Michael Pieschel <sixtyhectares@yahoo.com>
2018-08-29 02:02:07 +0000

    automatically backed up by learn

--- lib/school.rb
+++ lib/school.rb
@@ -1,15 +1,10 @@
 class School
-  attr_accessor :roster
-  attr_reader
-  ROSTER = {}
+
   def initialize(name)
     @name = name
   end
   def roster
-    @roster
+   roster = {}
   end
-  def roster=(roster)
-    @roster = roster
-    ROSTER << roster
-  end
+ 
 end
